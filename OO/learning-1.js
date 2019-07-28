f2124d9ca5c08dee6f692f6472fc4c2065c752f8
wsm <1030057982@qq.com>
2019-07-28 09:22:47 +0800

    学习 eslint 使用

---
+++ OO/learning-1.js
@@ -0,0 +1,185 @@
+// 多态
+// let GoogleMap = {
+//   show() {
+//     console.log('Google Map Beginning');
+//   }
+// };
+//
+// let BaiduMap = {
+//   show() {
+//     console.log('Baidu Map Beginning');
+//   }
+// };
+//
+// let renderMap = (map) => {
+//   if (map.show instanceof Function) {
+//     map.show();
+//   }
+// };
+//
+// renderMap(GoogleMap);
+// renderMap(BaiduMap);
+
+
+// 封装
+// 封装数据
+// var myObject = (function () {
+//   var __name = 'wsm';  // private 变量
+//   return {
+//     getName: function () {  // public 方法
+//       return __name;
+//     }
+//   };
+// })();
+//
+// console.log(myObject);
+
+// 原型模式
+// var Plane = function () {
+//   this.blood = 100;
+//   this.attacklevel = 1;
+//   this.defenselevel = 1;
+// };
+//
+// var plane = new Plane();
+// plane.blood = 66;
+// plane.attacklevel = 7;
+// plane.defenselevel = 5;
+//
+// var clonePlane = Object.create(plane);
+// console.log(clonePlane);
+
+// 闭包应用
+// var mult = (function () {
+//   var cache = {};
+//   var calculate = function () {
+//     var a = 1;
+//     for ( var i = 0, l = arguments.length ; i < l; i++) {
+//       a = a * arguments[i];
+//     }
+//     return a;
+//   };
+//   return function () {
+//     console.log(cache);
+//     var args = Array.prototype.join.call(arguments, ',');
+//     if (args in cache) {
+//       return cache[args];
+//     }
+//     return cache[args] = calculate.apply(null, arguments);
+//   };
+// })();
+//
+//
+// console.log(mult(1, 2, 3));
+// console.log(mult(1, 3, 2));
+
+// var appendDiv = function (callback) {
+//   for (var i = 0;i < 100; i++) {
+//     var div = document.createElement('div');
+//     div.innerHTML = i;
+//     document.body.appendChild(div);
+//     if (typeof callback === Function) {
+//       callback(div);
+//     }
+//   }
+// };
+//
+// appendDiv(function (node) {
+//   node.style.display = 'none';
+// });
+//
+
+// var isType = function (type) {
+//   return function (obj) {
+//     return Object.prototype.toString.call(obj) === `[object ${type}]`
+//   }
+// }
+//
+// var isString = isType('String').call(this)
+// var isArray = isType('Array')
+// var isNumber = isType('Number')
+//
+// console.log(isArray([1, 2, 3]))
+
+// Function.prototype.before = function (beforefn) {
+//   var __self = this    // 保留原函数引用
+//   return function () {   // 返回原函数和新函数的代理函数
+//     beforefn.apply(this, arguments)   // 执行新函数
+//     return __self.apply(this, arguments) // 执行原函数
+//   }
+// }
+//
+// Function.prototype.after = function (afterfn) {
+//   var __self = this
+//   return function () {
+//     var ret = __self.apply(this, arguments)
+//     afterfn.apply(this, arguments)
+//     return ret
+//   }
+// }
+//
+// func = function () {
+//   console.log(2)
+// }
+//
+// func = func.before(() => {console.log(1)}).after(() => {console.log(3)})
+
+// func()
+//
+// var throttle = function (fn, interval = 300) {
+//   var firstTime = true, timer
+//   return function () {
+//     if (firstTime) {
+//       fn.apply(this, arguments)
+//       firstTime = false
+//     }
+//     if (timer) return
+//     timer = setTimeout(() => {
+//       fn.apply(this, arguments)
+//       clearTimeout(timer)
+//       timer = null
+//     }, interval)
+//   }
+// }
+//
+// window.onresize = throttle(function () {
+//   console.log(1)
+// }, 500)
+
+// var timeChunk = function (ary, fn, count) {
+//   var t
+//
+//   var start = function () {
+//     for (var i = 0; i < Math.min(count || 1, ary.length); i++) {
+//       var obj = ary.shift()
+//       fn(obj)
+//     }
+//   }
+//
+//   return function () {
+//     t = setInterval(function () {
+//       if (ary.length === 0) {
+//         return clearInterval(t)
+//       }
+//       start()
+//     }, 200)
+//   }
+//
+// }
+//
+// var ary = []
+//
+// for (var i = 0; i < 15; i++){
+//   ary.push(i)
+// }
+//
+// var renderFriendList = timeChunk(ary, function () {
+//   console.log(1)
+// }, 8)
+//
+//
+// renderFriendList()
+
+
+
+
--- es6/examples/learning.js
+++ es6/examples/learning.js
@@ -20,5 +20,213 @@
 //     console.log(tmp);
 //   }
 // }
+//
 // f();
+// columns: [
+//   {
+//     title: '日期',
+//     slot: 'name',
+//     "fixed": "left",
+//     "width": 50,
+//
+//   },
+//   {
+//     title: '委托人',
+//     slot: 'age',
+//     "width": 90
+//   },
+//   {
+//     title: '公司联系人',
+//     slot: 'birthday',
+//     "width": 90
+//   },{
+//     title: '开船日',
+//     slot: 'birthday',
+//     "width": 90
+//   },
+//   {
+//     title: '地点',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '详细装货地点',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '箱型',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '承运人',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '船名',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '提单号',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '提单状态',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '箱号',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '封号',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '皮箱重量',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '目的港',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '提箱时间',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '提箱位置',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '状态',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '集港时间',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '码头',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '车牌号',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '司机电话',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '回箱位置',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '进/出',
+//     slot: 'address',
+//     "width": 90
+//   },{
+//     title: '费用',
+//     slot: 'address',
+//     "width": 90
+//   }
+// ],
+// data: [
+//   {
+//     name: '王小明',
+//     age: 18,
+//     birthday: '919526400000',
+//     address: '北京市朝阳区芍药居'
+//   },{
+//     name: '王小明',
+//     age: 18,
+//     birthday: '919526400000',
+//     address: '北京市朝阳区芍药居'
+//   },{
+//     name: '王小明',
+//     age: 18,
+//     birthday: '919526400000',
+//     address: '北京市朝阳区芍药居'
+//   },{
+//     name: '王小明',
+//     age: 18,
+//     birthday: '919526400000',
+//     address: '北京市朝阳区芍药居'
+//   },{
+//     name: '王小明',
+//     age: 18,
+//     birthday: '919526400000',
+//     address: '北京市朝阳区芍药居'
+//   },{
+//     name: '王小明',
+//     age: 18,
+//     birthday: '919526400000',
+//     address: '北京市朝阳区芍药居'
+//   },
+//   {
+//     name: '张小刚',
+//     age: 25,
+//     birthday: '696096000000',
+//     address: '北京市海淀区西二旗'
+//   },
+//   {
+//     name: '李小红',
+//     age: 30,
+//     birthday: '563472000000',
+//     address: '上海市浦东新区世纪大道'
+//   },
+//   {
+//     name: '周小伟',
+//     age: 26,
+//     birthday: '687024000000',
+//     address: '深圳市南山区深南大道'
+//   }
+// ],
+// editIndex: -1,  // 当前聚焦的输入框的行数
+// editName: '',  // 第一列输入框，当然聚焦的输入框的输入内容，与 data 分离避免重构的闪烁
+// editAge: '',  // 第二列输入框
+// editBirthday: '',  // 第三列输入框
+// editAddress: '',  // 第四列输入框
+
+// columns1: [
+//   {
+//     title: '状态',
+//     key: 'name'
+//   },
+//   {
+//     title: '提单号',
+//     key: 'age'
+//   },
+//   {
+//     title: '客户名',
+//     key: 'address'
+//   },{
+//     title: '箱号',
+//     key: 'address'
+//   },{
+//     title: '封号',
+//     key: 'address'
+//   },{
+//     title: '司机',
+//     key: 'address'
+//   },{
+//     title: '电话',
+//     key: 'address'
+//   },{
+//     title: '集港时间',
+//     key: 'address'
+//   },{
+//     title: '码头',
+//     key: 'address'
+//   },{
+//     title: '提箱时间',
+//     key: 'address'
+//   },{
+//     title: '提箱堆场',
+//     key: 'address'
+//   },{
+//     title: '回箱位置',
+//     key: 'address'
+//   },
+// ],
+
 
