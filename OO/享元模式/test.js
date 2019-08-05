92802c2842cc0b871be470a432db0fa62332f877
wsm <1030057982@qq.com>
2019-08-05 16:06:09 +0800

    享元模式

--- OO/享元模式/test.js
+++ OO/享元模式/test.js
@@ -65,3 +65,107 @@ startUpLoad('flash', [
     fileSize: 22000
   },
 ]);
+
+// 使用享元模式 进行重构
+// 区分 外部状态 和 内部状态
+// 内部状态储存于对象内部 可以被一些对象共享 独立于具体场景 通常不会被改变
+// 外部状态取决于具体场景 并根据场景变化而变化 外部状态不能被共享
+
+// 剥离外部状态
+var Upload = function (uploadType) {
+  this.uploadType = uploadType;
+};
+
+Upload.prototype.delFile = function (id) {
+  uploadManager.setExternalState(id, this);
+  if (this.fileSize < 3000) {
+    return this.dom.parentNode.removeChild(this.dom);
+  }
+  if (window.confirm('确定要删除该文件?' + this.fileName)) {
+    return this.dom.parentNode.removeChild(this.dom);
+  }
+};
+
+// 工厂进行对象实例化
+// 如果某种内部状态对应的共享对象已经被创建过 那么直接返回这个对象 否则则创建一个新对象
+
+var UploadFactory = (function () {
+  var createFlyWeightObjs = {};
+
+  return {
+    create: function (uploadType) {
+      if (createFlyWeightObjs[uploadType]) {
+        return createFlyWeightObjs[uploadType];
+      }
+      return createFlyWeightObjs[uploadType] = new Upload(uploadType);
+    }
+  };
+})();
+
+
+// 管理器封装外部状态
+// 负责向 UploadFactory 提交创建对象的请求 并用一个 uploadDatabase 对象保存所有 upload 对象的外部状态
+// 以便在程序运行过程中给 upload 共享对象设置外部状态
+
+var uploadManager = (function () {
+    var uploadDatabase = {};
+    return {
+      add: function (id, uploadType, fileName, fileSize) {
+        var flyWeightObj = UploadFactory.create(uploadType);
+        var dom = document.createElement('div');
+        dom.innerHTML = `<span>文件名称:${fileName},文件大小:${fileSize}</span>
+                         <button class="delFile">删除文件</button>`;
+        dom.querySelector('.delFile').onclick = function () {
+          flyWeightObj.delFile(id);
+        };
+        document.body.appendChild(dom);
+        uploadDatabase[id] = {
+          fileName: fileName,
+          fileSize: fileSize,
+          dom: dom
+        };
+
+        return flyWeightObj;
+      },
+
+      setExternalState: function (id, flyWeightObj) {
+        var uploadData = uploadDatabase[id];
+        for (var i in uploadData) {
+          flyWeightObj[i] = uploadData[i];
+        }
+      }
+    };
+})();
+
+var id = 0;
+
+window.startUpload = function (uploadType, files) {
+  for (var i = 0, file; file = files[i++];) {
+    var uploadObj = uploadManager.add(++id, uploadType, file.fileName, file.fileSize);
+  }
+};
+
+startUpload('plugin', [
+  {
+    fileName: '5.txt',
+    fileSize: 1000
+  },
+  {
+    fileName: '6.txt',
+    fileSize: 14000
+  },
+  {
+    fileName: '7.txt',
+    fileSize: 22000
+  }
+]);
+
+
+
+
+
+
+
+
+
+
